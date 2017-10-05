b5c89c232cc9d5134dce4a4c981f0a31277c3af3
Nithya Eswaran <eswarannm@s.dcsdk12.org>
2017-10-05 08:14:25 -0600

    add elevenish

--- methods2.rb
+++ methods2.rb
@@ -16,21 +16,163 @@
 
 
 # TODO - write elevenish?
+
+def elevenish?(n)
+	if n % 11 == 0
+		return true
+	end
+
+	if n % 11 == 1
+		return true
+	end
+
+	return false
+
+end
 	
 # TODO - write ice_cream_party?
 
-# TODO - write successful_squirrel_party?
+# def ice_cream_party(ice_cream, candy)
+# 	if ice_cream + candy < 5
+# 		return 	0
+# 	end
 
-# TODO - write ticket
+# 	if ice_cream + candy > 5
+# 		if ice_cream + candy < 10
+# 			return 1
+# 		end
+# 	end
 
-# TODO - write in_order?
+# 	if ice_cream + candy > 10 
+# 		return 2
+# 	end
 
-# TODO - write less_by_ten?
+# end
 
-# TODO - write fizz_string
+# # TODO - write successful_squirrel_party?
 
-# TODO - write first_last_six?
+# def successful_squirrel_party? (nuts, is_weekend)
+# 	if nuts > 40 
+# 		if nuts < 60
+# 			if is_weekend == false
+# 				return true
+# 			end
+# 		end
+# 	end
 
-# TODO - write rotate_left
 
-# TODO - write double23?
+# 	if nuts > 40 
+# 		if is_weekend == true
+# 			return true
+# 		end
+# 	end
+
+# 	if nuts < 40
+# 		if is_weekend == true
+# 			return false
+# 		end
+# 	end
+
+# 	if nuts < 40 || nuts > 60
+# 			if is_weekend == false
+# 				return false
+# 			end
+# 	end
+
+
+# end
+
+# # TODO - write ticket
+
+# def ticket (a, b, c)
+# 	ab = a + b
+# 	bc = b + c
+# 	ac = a + c
+
+# 	if ab == 10 || bc == 10 || ac == 10
+# 		return 10
+# 	end
+
+# 	if (ab == 10 + bc) || (ab == 10 + ac) 
+# 		return  5
+# 	end
+
+# 	return 0
+# end
+
+# # TODO - write in_order?
+# def in_order (a,b,c,bOK)
+
+# 	if bOK && c > b
+# 		return true
+# 	elsif b > a && c > b 
+# 		return true
+# 	end
+
+# 	return false
+		
+# end
+
+# # TODO - write less_by_ten?
+# def less_by_ten? (a,b,c)
+# 	return a - b >= 10 || b - a >= 10 || b - c >= 10 || c - b >= 10 || c - a >= 10 || a - c >= 10 
+
+# end
+
+# # TODO - write fizz_string
+# def fizz_string (string)
+
+# 	if string[0] == "f" || string[0] == "F" 
+# 		if string[(-1)] == "b" || string[(-1)] == "B"
+# 			return "FizzBuzz"
+# 		end
+# 	end
+
+# 	if string[0] == "f" || string[0] == "F"
+# 		return "Fizz"
+# 	end
+
+# 	if string[(-1)] == "b" || string[(-1)] == "B"
+# 		return "Buzz"
+# 	end
+
+# end
+
+# # TODO - write first_last_six?
+
+# def first_last_six?(array)
+# 	if array[0] == 6 || array[(-1)] == 6
+# 		return true
+# 	end
+
+# 	return false
+# end
+# # TODO - write rotate_left
+
+# def rotate_left(trio)
+# 	a = trio[0]
+# 	b = trio[1]
+# 	c = trio[2]
+
+# 	return [b,c,a]
+# end
+
+# # TODO - write double23?
+
+# def double23?(array)
+# 	a = array[0]
+# 	b = array[1]
+# 	c = array[2]
+
+# 	if a == 2 && b == 2 || a == 3 && b == 3
+# 		return true
+# 	end
+
+# 	if c == 2 && b == 2 || c == 3 && b == 3
+# 		return true
+# 	end
+
+# 	return false
+
+# end
+
--- test.rb
+++ test.rb
@@ -2,8 +2,176 @@ require './test_setup'
 require './methods2'
 
 
-describe 'Methods2' do
+describe 'methods2' do
 
-  # TODO - write tests for each method in methods2.rb
+	describe 'elevenish?' do
+		it 'factor of 11' do
+			elevenish?(121).must_equal(true)
+		end
+
+		it 'factor of 11 plus 1' do
+			elevenish?(122).must_equal(true)
+		end
+
+		it 'not a factor of 11' do
+			elevenish?(87).must_equal(false)
+		end
+
+	end
+
+	# describe 'ice_cream_party' do
+	# 	it 'bad party' do
+	# 		ice_cream_party(1,3).must_equal(0)
+	# 	end
+
+	# 	it 'good party' do
+	# 		ice_cream_party(4,3).must_equal(1)
+	# 	end
+
+	# 	it 'great party' do
+	# 		ice_cream_party(7,9).must_equal(2)
+	# 	end
+
+	# end
+
+	# describe 'successful_squirrel_party?' do
+	# 	it 'good party weekday' do
+	# 		successful_squirrel_party?(45,false).must_equal(true)
+	# 	end
+
+	# 	it 'bad party weekday' do
+	# 		successful_squirrel_party?(30,false).must_equal(false)
+	# 	end
+
+	# 	it 'good party weekend' do
+	# 		successful_squirrel_party?(80,true).must_equal(true)
+	# 	end
+
+	# 	it 'bad party weeked' do
+	# 		successful_squirrel_party?(30,true).must_equal(false)
+	# 	end
+
+	# end
+
+	# describe 'ticket' do
+	# 	it 'win lottery' do
+	# 		ticket(3,7,1).must_equal(10)
+	# 	end
+
+	# 	it 'win half' do
+	# 		ticket(12,4,2).must_equal(5)
+	# 	end
+
+	# 	it 'lose lottery' do
+	# 		ticket(1,1,1).must_equal(0)
+	# 	end
+
+	# end
+
+	# describe 'in_order' do
+	# 	it 'order false' do
+	# 		in_order(4,7,6,false).must_equal(false)
+	# 	end
+
+	# 	it 'order true' do
+	# 		in_order(4,3,10,true).must_equal(true)
+	# 	end
+
+	# 	it 'no order, true' do
+	# 		in_order(10,9,3,true).must_equal(false)
+	# 	end
+	# 	it 'no order, false' do
+	# 		in_order(7,4,9,false).must_equal(false)
+	# 	end
+	# end
+
+	# describe 'less_by_ten?' do
+	# 	it 'true' do
+	# 		less_by_ten?(2,12,17).must_equal(true)
+	# 	end
+
+	# 	it 'false' do
+	# 		less_by_ten?(2,3,4).must_equal(false)
+	# 	end
+
+	# end
+
+	# describe 'fizz_string' do
+	# 	it 'Just Fizz' do
+	# 		fizz_string("Friend").must_equal("Fizz")
+	# 	end
+
+	# 	it 'Just Buzz' do
+	# 		fizz_string("Bob").must_equal("Buzz")
+	# 	end
+	# 	it 'Both Fizz Buzz' do
+	# 		fizz_string("Friendbob").must_equal("FizzBuzz")
+	# 	end
+
+
+	# end
+
+	# describe 'first_last_six' do
+
+	# 	it 'first six' do
+	# 		first_last_six?([6,0,3,7,4,6,4,3]).must_equal(true)
+	# 	end
+
+	# 	it 'last six' do
+	# 		first_last_six?([0,5,203,45,6]).must_equal(true)
+	# 	end
+
+	# 	it 'Both first and last' do
+	# 		first_last_six?([6,6,6,6,6,6,6,6,6,6]).must_equal(true)
+	# 	end
+
+	# 	it 'neither first and last' do
+	# 		first_last_six?([2,3,5,3,23,4,3,2,3]).must_equal(false)
+	# 	end
+	# end
+
+	# describe 'rotate_left' do
+
+	# 	it '6,0,3' do
+	# 		rotate_left([6,0,3]).must_equal([0,3,6])
+	# 	end
+
+	# 	it '0,5,203' do
+	# 		rotate_left([0,5,203]).must_equal([5,203,0])
+	# 	end
+
+	# 	it '6,6,6' do
+	# 		rotate_left([6,6,6]).must_equal([6,6,6])
+	# 	end
+
+	# end
+
+	# describe 'double23?' do
+	# 	it '1' do
+	# 		double23?([1]).must_equal(false)
+	# 	end
+
+	# 	it '2 twice' do
+	# 		double23?([2,2]).must_equal(true)
+	# 	end
+
+	# 	it '3 twice' do
+	# 		double23?([3,3]).must_equal(true)
+	# 	end
+
+	# 	it 'empty' do
+	# 		double23?([]).must_equal(false)
+	# 	end
+
+	# 	it '0' do
+	# 		double23?([0]).must_equal(false)
+	# 	end
+
+	# 	it 'false' do
+	# 		double23?([2,3]).must_equal(false)
+	# 	end
+
+	# end
 
 end
+
